eaa395cee7b322e26f9f4eb4cccb4a377fc6dd99
Ilya Stroev <ilya_stroev@mail.ru>
2023-03-08 20:33:33 +0300

    дз 3 выполнил 1 задачу.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+        int first = 1;
+        byte second = 2;
+        short third = 3;
+        long fourth = 4;
+        float fifth = 5.0F;
+        double sixth = 6.0d;
+        System.out.println("Значение переменной first с типом int равно " + first );
+        System.out.println("Значение переменной second с типом byte равно " + second );
+        System.out.println("Значение переменной third с типом short равно " + third );
+        System.out.println("Значение переменной fourth с типом long равно " + fourth );
+        System.out.println("Значение переменной fifth с типом float равно " + fifth );
+        System.out.println("Значение переменной sixth с типом double равно " + sixth );
     }
 }
