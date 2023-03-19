c77029f43c4b6f31e0146afaa87f590e24113140
Ilya Stroev <ilya_stroev@mail.ru>
2023-03-19 09:29:53 +0300

    Выполнил 2 задачу в 5 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -32,13 +32,46 @@ public class Main {
 
     private static void task2() {
         System.out.println("Задача 2");
-
-
+        int clientOS = 0;
+        int yearOS = 2014;
+        int year = 2015;
+        switch (clientOS){
+            case 0 :
+                if (yearOS>=year) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1 :
+                if(yearOS>=year) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Данная ОС не поддерживается!");
+        }
 
     }
 
+
+
+
     private static void task1() {
         System.out.println("Задача 1");
+        int clientOS = 0;
+        switch (clientOS){
+            case 0 :
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1 :
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Данная ОС не поддерживается!");
+        }
 
     }
 }
