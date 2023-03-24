4215c1adfc7601287c36f6b2556a3ea8993812bd
Ilya Stroev <ilya_stroev@mail.ru>
2023-03-24 15:12:29 +0300

    Выполнил 8 задачу 7 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,17 @@ public class Main {
 
     private static void task7() {
         System.out.println("Задача 7");
-
+        int firstFriday = 10;
+        int countMouth =1;
+        int remains = 0;
+        int i = 1;
+        while (countMouth <= 12) {
+            for (i=firstFriday+remains; i <= 31; i = i + 7) {
+                System.out.println("Сегодня пятница, "+ i +"-е число. Необходимо подготовить отчет");
+            }
+            remains = i- 31-firstFriday;
+            countMouth++;
+        }
     }
 
     private static void task6() {
