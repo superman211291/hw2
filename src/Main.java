1fc4549c99f88ce3171abdd5b55318c9b6bbdaf7
Ilya Stroev <ilya_stroev@mail.ru>
2023-03-25 15:35:26 +0300

    Поправил 4,5,6 задачу 7 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,14 @@ public class Main {
     }
 
 
-
     private static void task8() {
         System.out.println("Задача 8");
         int before200Year = 2023 - 200;
         int after100Year = 2023 + 100;
         int year = 0;
-        while (year <= after100Year){
+        while (year <= after100Year) {
             year = year + 79;
-            if (year>= before200Year && year<=after100Year){
+            if (year >= before200Year && year <= after100Year) {
                 System.out.println(year);
             }
         }
@@ -30,14 +29,14 @@ public class Main {
     private static void task7() {
         System.out.println("Задача 7");
         int firstFriday = 10;
-        int countMouth =1;
+        int countMouth = 1;
         int remains = 0;
         int i;
         while (countMouth <= 12) {
-            for (i=firstFriday+remains; i <= 31; i = i + 7) {
-                System.out.println("Сегодня пятница, "+ i +"-е число. Необходимо подготовить отчет");
+            for (i = firstFriday + remains; i <= 31; i = i + 7) {
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
             }
-            remains = i- 31-firstFriday;
+            remains = i - 31 - firstFriday;
             countMouth++;
         }
     }
@@ -45,13 +44,12 @@ public class Main {
     private static void task6() {
         System.out.println("Задача 6");
         float percent = 7;
-        float money = 15_000;
-        float sum = 0;
-        float countMounth = 0 ;
-        while (countMounth< 9 * 12){
-            sum=money + sum + ((money + sum)/100)*(percent/12);
+        float sum = 15_000;
+        float countMounth = 0;
+        while (countMounth < 9 * 12) {
+            sum = sum + (sum / 100 * percent);
             countMounth++;
-            if (countMounth%6==0 || countMounth> 9 * 12 ) {
+            if (countMounth % 6 == 0 || countMounth > 9 * 12) {
                 System.out.println("Сумма накоплений в " + countMounth + " месяц - " + sum);
             }
         }
@@ -61,14 +59,13 @@ public class Main {
     private static void task5() {
         System.out.println("Задача 5");
         float percent = 7;
-        float money = 15_000;
         float finishSum = 12_000_000;
-        float sum = 0;
-        int countMounth =0 ;
-        while (sum < finishSum){
-            sum=money + sum + ((money + sum)/100)*(percent/12);
+        float sum = 15_000;
+        int countMounth = 0;
+        while (sum < finishSum) {
+            sum = sum + (sum / 100 * percent);
             countMounth++;
-            if (countMounth%6==0 || sum > finishSum) {
+            if (countMounth % 6 == 0 || sum > finishSum) {
                 System.out.println("Сумма накоплений в " + countMounth + " месяц - " + sum);
             }
         }
@@ -78,14 +75,13 @@ public class Main {
     private static void task4() {
         System.out.println("Задача 4");
         float percent = 7;
-        float money = 15_000;
         float finishSum = 12_000_000;
-        float sum = 0;
-        int countMounth =0 ;
-        while (sum < finishSum){
-            sum=money + sum + ((money + sum)/100)*(percent/12);
+        float sum = 15_000;
+        int countMounth = 0;
+        while (sum < finishSum) {
+            sum = sum + (sum / 100 * percent);
             countMounth++;
-            System.out.println("Сумма накоплений в " + countMounth +" месяц - "+sum);
+            System.out.println("Сумма накоплений в " + countMounth + " месяц - " + sum);
         }
 
     }
@@ -95,21 +91,21 @@ public class Main {
         int humans = 12_000_000;
         int come = 17; //17/1000
         int died = 8; //8/1000
-        int sumComeHumans ;
-        int sumDiedHumans ;
+        int sumComeHumans;
+        int sumDiedHumans;
         for (int i = 1; i <= 10; i++) {
-            sumComeHumans = (humans / 1000)*come;
-            sumDiedHumans = (humans/1000)*died;
-            humans = humans+sumComeHumans-sumDiedHumans;
-            System.out.println("Год "+ i +", численность населения составляет "+humans);
+            sumComeHumans = (humans / 1000) * come;
+            sumDiedHumans = (humans / 1000) * died;
+            humans = humans + sumComeHumans - sumDiedHumans;
+            System.out.println("Год " + i + ", численность населения составляет " + humans);
         }
     }
 
     private static void task2() {
         System.out.println("Задача 2");
-        int i=1;
-        while (i<10){
-            System.out.print(i +" ");
+        int i = 1;
+        while (i < 10) {
+            System.out.print(i + " ");
             i++;
         }
         System.out.println();
@@ -123,11 +119,11 @@ public class Main {
         System.out.println("Задача 1");
         int money = 15_000;
         int summ = 0;
-        int mounthCount =0;
-        while (summ < 2_459_000){
-            summ = summ+money;
+        int mounthCount = 0;
+        while (summ < 2_459_000) {
+            summ = summ + money;
             mounthCount++;
-            System.out.println("Месяц "+ mounthCount +", сумма накоплений равна "+ summ +" рублей");
+            System.out.println("Месяц " + mounthCount + ", сумма накоплений равна " + summ + " рублей");
         }
 
     }
