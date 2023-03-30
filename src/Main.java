7d4d96ec51f60b4ea4c48854fbac3b33e6dda009
Ilya Stroev <ilya_stroev@mail.ru>
2023-03-30 19:34:02 +0300

    Создал 9 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
     }
 }
