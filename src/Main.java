20808f64e63498ec94534b39915de7e804705731
Ilya Stroev <ilya_stroev@mail.ru>
2023-04-01 15:40:29 +0300

    Создал 9 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    private static void task4() {
+        System.out.println("Задача 4");
+    }
+    private static void task3() {
+        System.out.println("Задача 3");
+    }
+    private static void task2() {
+        System.out.println("Задача 2");
     }
 
     private static void task1() {
         System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
     }
 }
