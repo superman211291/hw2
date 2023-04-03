7e07f24516044fa9ac4b7e30f54c99ba98960065
Ilya Stroev <ilya_stroev@mail.ru>
2023-04-03 16:43:54 +0300

    Выполнил 2 задачу 10 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,9 @@ public class Main {
 
     private static void task2() {
         System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
     }
 
     private static void task1() {
