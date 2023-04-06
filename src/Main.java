42384a7ffee3eb105e2906040601f405c9fb1b93
Ilya Stroev <ilya_stroev@mail.ru>
2023-04-06 11:19:35 +0300

    Выполнил 2 задачу 11 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         task1(2021);
+        task2(0,2014);
+    }
+
+    private static void task2(int clientOS, int yearOS) {
+        System.out.println("Задача 2");
+        int year = 2015;
+        switch (clientOS){
+            case 0 :
+                if (yearOS>=year) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1 :
+                if(yearOS>=year) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Данная ОС не поддерживается!");
+        }
+
     }
 
     private static void task1(int year) {
