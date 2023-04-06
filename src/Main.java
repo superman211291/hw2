4fb580c6d04e5f3e1e5bdb6b8419def69db492f3
Ilya Stroev <ilya_stroev@mail.ru>
2023-04-06 11:13:29 +0300

    Выполнил 1 задачу 11 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1(2021);
+    }
+
+    private static void task1(int year) {
+        System.out.println("Задача 1");
+        if ((year%100!=0 && year%4==0) || year%400==0){
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
     }
 }
